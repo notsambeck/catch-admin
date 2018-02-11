bf9fef0a12a91d301483a864d416cd0370ec06ce
thesamuelbeck@gmail.com <thesamuelbeck@gmail.com>
2018-02-11 20:02:02 +0000

    Saved automatically

--- server_modules/ServerModule1.py
+++ server_modules/ServerModule1.py
@@ -4,13 +4,21 @@ from tables import app_tables
 import anvil.server
 from datetime import datetime
 
+from collections import defaultdict
+
 @anvil.server.callable
 def get_user_action_counts(user):
   return len(app_tables.user_actions.search(user=user))
 
-def get_user_actions():
+def get_traffic():
   '''
-  gets a dictionary of user actions:
-  
+  gets a dictionary:
+  {time bucket (hour): number of actions in bucket}
   '''
-  
+  out = defaultdict(0)
+  now = datetime.utcnow()
+  data = app_tables.user_actions.search(tables.order_by('time', ascending=False))
+  for row in data:
+    out[(now - row['time']).hours] += 1
+    
+  return out
