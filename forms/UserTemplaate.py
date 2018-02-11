63bc692858b0d791c3d0d784c085a2c9b4657631
thesamuelbeck@gmail.com <thesamuelbeck@gmail.com>
2018-02-11 22:50:19 +0000

    dumb user engagement metrics work

--- forms/UserTemplaate.py
+++ forms/UserTemplaate.py
@@ -3,8 +3,25 @@ import anvil.server
 import anvil.users
 import tables
 from tables import app_tables
+from datetime import datetime
 
 class UserTemplaate (UserTemplaateTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
+
+  def form_show (self, **event_args):
+    # This method is called when the column panel is shown on the screen
+    my_actions = self.parent.parent.actions.search(user=self.item)
+    
+    account_created = self.item['account_created']
+    self.throws_label.text = len(my_actions)
+    
+    time_active = 0
+    for action in my_actions:
+      delta = (action['time'] - account_created).days
+      if delta > time_active:
+        time_active = delta
+    self.date_label.text = 'time active: {} days'.format(time_active)
+    
+    self.logins_label.text = len(self.parent.parent.actions.search(user=self.item, action_type='login'))
--- forms/Users.py
+++ forms/Users.py
@@ -7,8 +7,19 @@ from tables import app_tables
 class Users (UsersTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
+    self.test_on = False
     self.init_components(**properties)
-    self.user_data = app_tables.catch_users.client_readable()
-    self.action_data = app_tables.user_actions.client_readable()
-    self.repeating_panel_1.items = self.user_data.search()
-    # Any code you write here will run when the form opens.
+    self.users = app_tables.catch_users.client_readable()
+    self.actions = app_tables.user_actions.client_readable()
+    
+  def button_1_click (self, **event_args):
+    # This method is called when the button is clicked
+    self.test_on = not self.test_on
+    self.repeating_panel_1_show()
+
+  def repeating_panel_1_show (self, **event_args):
+    # This method is called when the Label is shown on the screen
+    if self.test_on:
+      self.repeating_panel_1.items = self.users.search()
+    else:
+      self.repeating_panel_1.items = self.users.search(user_type='')
