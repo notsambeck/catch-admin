f68b14afb54c15d8bf044baf8e302166fc27e6f3
thesamuelbeck@gmail.com <thesamuelbeck@gmail.com>
2018-02-11 19:52:37 +0000

    Saved automatically

--- forms/UserTemplaate.py
+++ forms/UserTemplaate.py
@@ -8,9 +8,3 @@ class UserTemplaate (UserTemplaateTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
-
-  def button_1_click (self, **event_args):
-    # This method is called when the button is clicked
-    count = self.parent.parent.action_data.search(user=self.item)
-    self.label_2.text = count
-    # Any code you write here will run when the form opens.
--- server_modules/ServerModule1.py
+++ server_modules/ServerModule1.py
@@ -2,7 +2,15 @@ import anvil.users
 import tables
 from tables import app_tables
 import anvil.server
+from datetime import datetime
 
 @anvil.server.callable
 def get_user_action_counts(user):
   return len(app_tables.user_actions.search(user=user))
+
+def get_user_actions():
+  '''
+  gets a dictionary of user actions:
+  
+  '''
+  
